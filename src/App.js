b4b8655d8a9b1f481896b08c465068589b3bd01c
Dosbolacademic <0770478767d@gmail.com>
2023-02-27 19:31:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+
+import { useState, useEffect } from "react";
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState(""); //For Inputs
+  const [recipes, setRecipes] = useState([]); //We want to store Recipes here
+
+  // function to search for the recipes
+
+  const searchRecipes = async () => {
+    //Why do we need ASYNC?????
+    setIsLoading(true);
+    const url = apiUrl + query;
+    const res = await fetch(url); //Why do we need FETCH???
+    const data = await res.json();
+    //console.log(data)
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  }, []);
+
+  const handleSubmit=event=>{
+    event.preventDefault()
+    searchRecipes()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Recipes for Dishes</h2>
+      <SearchBar 
+      handleSubmit={handleSubmit}
+      value={query}
+      onChange={event=>setQuery(event.target.value)}
+      isLoading={isLoading}
+
+
+
+    />
+
+
+
+
+
+
+      <div className="recipes">
+        {recipes
+          ? recipes.map((recipe) => (
+              <RecipeCard key={recipe.idMeal} recipe={recipe} />
+            ))
+          : "No Recipes!"}
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SearchBar = ({
+  value,
+  isLoading,
+  handleSubmit,
+  onChange
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={value}
+        disabled={isLoading}
+        onChange={onChange}
+        placeholder="Search Recipes"
+        className="form-control"
+      />
+      <input
+        disabled={isLoading || !value}
+        type='submit'
+        className="btn"
+        value="Search"
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
